c07edc798f95bd11201297a811ce316c67d753da
Hemant Sah <hemantsah2912@gmail.com>
2025-02-20 20:24:45 +0530

    pase-1 done

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+import {reviewData} from "./reviewData.js"
+console.log(reviewData);
 
 const userName = document.getElementById("name");
 const userReview = document.getElementById("review");
 const submitBtn = document.getElementById("submit-btn");
+const reviewList = document.getElementById("review-lists");
+
+reviewData.forEach((data)=>renderReview(data));
 
 submitBtn.addEventListener("click",storeReview);
-
-
 function storeReview(){
     let name = userName.value.trim();
     let review = userReview.value.trim();
-    console.log(name, review);
     
     if(!name && !review){
         return;
@@ -21,16 +23,27 @@ function storeReview(){
     const user = {
         name:name,
         review:review,
+        rating:"4⭐",
         time:formatted
     }   
     userName.value = "";
-    userReview.value = "";
-
-    displayReview(user);
+    userReview.value = "";   
+    reviewData.push(user);
+    console.log(reviewData);
+    renderReview(user);
 }
 
-function displayReview(review){
-    const li = createElement("li");
-    li.setAttribute("class","review-list");
+function renderReview(data){   
+    const {name, review, rating, time} = data;
     
+    const li = document.createElement("li");
+    li.innerHTML = `<div class="person">
+                        <p>${name}</p>
+                        <span id="time">${time}</span>
+                    </div>
+                    <div class="person-review">
+                        <p>${review}</p>
+                        <span>${rating}</span>
+                    </div>`   
+    reviewList.prepend(li);
 }
