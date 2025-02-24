313edbef86b0c0350bd7755176404487e2b350f3
Hemant Sah <hemantsah2912@gmail.com>
2025-02-24 13:35:17 +0530

    scrolling-update

--- reviewData.js
+++ reviewData.js
@@ -23,5 +23,29 @@ const reviewData = [
         rating:5,
         time:"2/20/2025, 12:45:12 PM"
     },
+    {
+        name:"Pratik Prakhar",
+        review:"The Review-Collector is good, but there should be an option to filter spam or fake reviews automatically.",
+        rating:5,
+        time:"2/18/2025, 12:00:10 AM"
+    },
+    {
+        name:"Piyush Nayak",
+        review:"The UI is decent, but the loading speed needs improvement. Sometimes, pages take too long to load.",
+        rating:4,
+        time:"2/19/2025, 4:50:02 AM"
+    },
+    {
+        name:"Haresh Gupta",
+        review:"Overall, a solid app! Adding sort review by rating would make it even better.",
+        rating:5,
+        time:"2/19/2025, 11:56:20 PM"
+    },
+    {
+        name:"Shrey Sahay",
+        review:"Fantastic app! I love how easy it is to collect and manage reviews. The interface is clean and simple!",
+        rating:5,
+        time:"2/20/2025, 12:45:12 PM"
+    },
 ]
 export {reviewData}
