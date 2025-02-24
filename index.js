8068df52400c68b6bf1fb3c76d09f70919731746
Hemant Sah <hemantsah2912@gmail.com>
2025-02-24 12:46:04 +0530

    frontend-end done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const userReview = document.getElementById("review");
 const submitBtn = document.getElementById("submit-btn");
 const reviewList = document.getElementById("review-lists");
 const starIconList = document.getElementById("rating-icon");
-
+let ratingNum = 0;
 reviewData.forEach((data)=>renderReview(data));
 
 submitBtn.addEventListener("click",storeReview);
@@ -13,7 +13,7 @@ function storeReview(){
     let name = userName.value.trim();
     let review = userReview.value.trim();
     
-    if(!name && !review){
+    if(!name || !review){
         return;
     }
 
@@ -23,13 +23,14 @@ function storeReview(){
     const user = {
         name:name,
         review:review,
-        rating:"4⭐",
+        rating:ratingNum,
         time:formatted
     }   
     userName.value = "";
-    userReview.value = "";   
+    userReview.value = "";
+    ratingNum=0;
+    removeStarRating();
     reviewData.push(user);
-    console.log(reviewData);
     renderReview(user);
 }
 
@@ -38,6 +39,17 @@ function renderReview(data){
     const li = document.createElement("li");
     const shortReview = review.length > 50 ? review.substring(0, 50) + "..." : review;
     const btnText = shortReview === review ? "":  "more";
+    let ratingStarString = "";
+    let notRatingStarString = "";
+    for(let star=0; star<rating; star++){
+        ratingStarString += "⭐"
+    }
+    if(rating<5){
+        for(let notstar=rating; notstar<5; notstar++){
+            notRatingStarString += "⭐"
+        }
+    }
+
     li.innerHTML = `<div class="person">
                         <p>${name}</p>
                         <span id="time">${time}</span>
@@ -45,7 +57,10 @@ function renderReview(data){
                     <div class="person-review">
                         <p class="short-review">${shortReview}<button class="more-btn">${btnText}</button></p>
                         <p class="full-review hidden">${review}<button class="less-btn">less</button></p>
-                        <span>${rating}</span>
+                        <div class="rating-star">
+                            <span>${ratingStarString}</span>
+                            <span class="not-rate">${notRatingStarString}</span>
+                        </div>
                     </div>`   
     reviewList.prepend(li);
 
@@ -64,23 +79,21 @@ function renderReview(data){
     });
 }
 
-// document.getElementById("star-icon").addEventListener("click",()=>{
-//     document.getElementById("star-icon").classList.add("rate");
-// })
-// const starList = starIconList.querySelectorAll(".star-icon");
-// console.log("hello", starList);
-// const ratingIcon = document.getElementById("rating-icon");
-
-//solve this: prbm with className and queryselector. check what className and query selector return.
-
 starIconList.addEventListener("click",(e)=>{
-    const ratingStar = +e.target.dataset.id;
-    const starIcons = document.getElementsByClassName("star-icon");
-    console.log(starIcons.length);
-    for(let i=0; i<ratingStar; i++){
-        console.log(starIcons[i]);
-        
+    //1st remove all the previous rating
+    removeStarRating();
+    
+    //add rating
+    // const ratingStar = +e.target.dataset.id;
+    ratingNum = +e.target.dataset.id;
+    for(let i=0; i<ratingNum; i++){
+        document.getElementById(`star-${i+1}`).classList.add("rate");      
     }
-    e.target.classList.add("rate");
 })
+function removeStarRating(){
+    const allStarIcon = document.getElementsByClassName("star-icon");    
+    for(let star of allStarIcon){
+        star.classList.remove("rate");
+    }
+}
 
