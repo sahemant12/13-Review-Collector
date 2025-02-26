4e0a503decb6ed7d7b64f5d0941768b2f5265c1c
Hemant Sah <hemantsah2912@gmail.com>
2025-02-26 19:20:28 +0530

    add url

--- frontend/index.js
+++ frontend/index.js
@@ -10,7 +10,7 @@ let ratingNum = 0;
 //get review
 const getReviews = async () => {
     try{
-        const res = await axios.get('/reviews');
+        const res = await axios.get('http://localhost:8080/reviews');
         return res.data;
     }catch(error){
         console.log(error);
@@ -21,7 +21,7 @@ const getReviews = async () => {
 
 //add review
 const addReview = async(review)=>{
-    const res = await axios.post('/review',review);
+    const res = await axios.post('http://localhost:8080/review',review);
     console.log(res.data);
 }
 const reviewDataDB = await getReviews();
