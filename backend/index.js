429a2aa106278604a1abb9a7b0ae6337280cda57
Hemant Sah <96417076+sahemant12@users.noreply.github.com>
2025-02-27 02:15:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,6 @@ const app = express();
 app.use(cors({
   origin: ["https://13-review-collector-frontend.vercel.app"],
   methods: ["POST", "GET"],
-  credentials:true,
 }));
 app.use(express.json());
 // app.use(cors());
